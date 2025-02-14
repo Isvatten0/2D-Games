7c73db9d4278ccc2cc7fc31d7a8d87dbec228350
luker <luke.roberts047@gmail.com>
2025-02-14 11:55:07 -0500

    camera added and player centered

--- 5games-main/Vampire survivor/code/main.py
+++ 5games-main/Vampire survivor/code/main.py
@@ -4,17 +4,18 @@ from player import Player
 from sprites import *
 from random import randint
 from pytmx.util_pygame import load_pygame
+from groups import AllSprites
 
 class Game:
     def __init__(self):
         # initial setup
         pygame.init()
-        self.display_surface = pygame.display.set_mode((WINDOW_ WIDTH,WINDOW_HEIGHT))
+        self.display_surface = pygame.display.set_mode((WINDOW_WIDTH,WINDOW_HEIGHT))
         pygame.display.set_caption('Vampire Survival')
         self.clock = pygame.time.Clock()
         self.running = True
         # groups
-        self.all_sprites = pygame.sprite.Group()
+        self.all_sprites = AllSprites()
         self.collision_sprites = pygame.sprite.Group()
         self.setup()
 
@@ -27,8 +28,15 @@ class Game:
 
     def setup(self):
         map = load_pygame(join('Vampire survivor', 'data', 'maps', 'world.tmx'))
+
+        for x,y,image in map.get_layer_by_name('Ground').tiles():
+            Sprite((x * TILE_SIZE,y * TILE_SIZE),image,self.all_sprites)
+
         for objects in map.get_layer_by_name('Objects'): # this function is the most common way to get a layer
-               CollisionSprite((objects.x,objects.y),objects.image,(self.all_sprites,self.collision_sprites))
+            CollisionSprite((objects.x,objects.y),objects.image,(self.all_sprites,self.collision_sprites))
+
+        for collisions in map.get_layer_by_name('Collisions'):
+            CollisionSprite((collisions.x,collisions.y), pygame.Surface((collisions.width,collisions.height)),self.collision_sprites)
     def run(self):
         while self.running:
             # delta time
@@ -43,7 +51,7 @@ class Game:
 
             # draw 
             self.display_surface.fill('black')
-            self.all_sprites.draw(self.display_surface)
+            self.all_sprites.draw(self.player.rect.center)
             pygame.display.update()
         pygame.quit()
 
